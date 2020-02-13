dfa3868197c3e61b92c907c1288a96f2a8eaf296
abhay-singh-au3 <52338948+abhay-singh-au3@users.noreply.github.com>
2020-02-13 14:51:31 +0530

    validation signup fix

--- client/src/components/validateSignup.js
+++ client/src/components/validateSignup.js
@@ -1,22 +1,22 @@
 export default function validate(user) {
     let errors = {}
 
-    if (!user.firstName) {
+    if (user.firstName.trim().length < 1) {
         errors.firstName = "Firstname cannnot be empty"
     } else if (user.firstName.length > 25) {
         errors.firstName = "Should be less than 25 chars"
     }
-    if (!user.lastName) {
+    if (user.lastName.trim().length < 1) {
         errors.lastName = "Lastname cannnot be empty"
     } else if (user.lastName.length > 25) {
         errors.lastName = "Should be less than 25 chars"
     }
-    if (!user.email) {
+    if (user.email.trim().length < 1) {
         errors.email = "Email Cannot be empty"
     } else if (!/\S+@\S+/.test(user.email)) {
         errors.email = "Email address is invalid"
     }
-    if (!user.password) {
+    if (user.password.trim().length < 1) {
         errors.password = "Password is required"
     }
     if (user.password !== user.confirmPass) {
