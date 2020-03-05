54710fb5d1554af8cb6fab0ab0982cd0a2eef30c
abhay-singh-au3 <52338948+abhay-singh-au3@users.noreply.github.com>
2020-03-05 16:21:41 +0530

    frontend for booking places

--- client/src/components/Expdetails.js
+++ client/src/components/Expdetails.js
@@ -1,14 +1,22 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Carousel } from 'react-bootstrap';
 import axios from 'axios';
-import { NavDropdown, Navbar, Nav } from "react-bootstrap";
+import { NavDropdown, Navbar, Nav, Modal } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const Expdetails = props => {
+    const [modal, setModal] = useState(false)
+    const openModal = () => setModal(true)
+    const closeModal = () => setModal(false)
+    const [persons, setPersons] = useState("")
     const [data, setData] = useState(null)
     useEffect(() => {
-        axios.get(`/exp/${props.match.params.id}`).then(res => setData(res.data))
+        axios.get(`/exp/${props.match.params.id}`).then(res => {
+            setData(res.data)
+        })
     }, [])
     const history = useHistory();
     const onLogout = () => {
@@ -28,6 +36,22 @@ const Expdetails = props => {
     const handleEditProfile = () => {
         history.push("/editProfile", { type: "user" });
     };
+    const handleChange = e => {
+        setPersons(e.target.value)
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        axios.post("/book/exp", {
+            total: persons * data.price,
+            city: data.city,
+            zip: data.zip
+        }).then(res => {
+            closeModal();
+            toast.success("Details sent to your email")
+        }).catch(err => {
+            toast.error("Something's wrong.. Apoligies")
+        })
+    }
     return (
         <Fragment>
             <nav>
@@ -60,8 +84,8 @@ const Expdetails = props => {
                                 <div className="col-md-6 offset-md-3">
                                     <Carousel>
                                         {
-                                            data.images.map(url =>
-                                                <Carousel.Item>
+                                            data.images.map((url, idx) =>
+                                                <Carousel.Item key={idx}>
                                                     <img
                                                         className="d-block w-100"
                                                         alt="..."
@@ -77,17 +101,40 @@ const Expdetails = props => {
                         <div className="container">
                             <div className="row">
                                 <div className="col text-center details">
-                                <h2 className="h5">{data.header}</h2>
+                                    <h2 className="h5">{data.header}</h2>
                                     <p className="lead">{data.description}</p>
                                     <p>{data.category} <small className="text-muted">category</small></p>
-                                    <p className="lead"><strong>${data.price}</strong> / night</p>
+                                    <p className="lead"><strong>${data.price}</strong></p>
                                     <p className="lead">{data.city} - {data.state} - {data.zip}</p>
-                                    <button className="btn btn-danger">Book Now</button>
-                    </div>
+                                    <button className="btn btn-danger" onClick={openModal}>Book Now</button>
+                                </div>
                             </div>
                         </div>
                     </div>
             }
+            <Modal show={modal} onHide={closeModal} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Fill details for booking</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group row">
+                            <label htmlFor="persons" className="col-6 col-form-label">Total persons</label>
+                            <div className="col-6">
+                                <input
+                                    className="form-control"
+                                    type="number"
+                                    name="persons"
+                                    value={persons}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <button className="btn btn-danger" type="submit">Confirm Booking</button>
+                    </form>
+                </Modal.Body>
+            </Modal>
+            <ToastContainer />
         </Fragment>
     );
 }
--- client/src/components/Placedetail.js
+++ client/src/components/Placedetail.js
@@ -1,14 +1,23 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Carousel } from 'react-bootstrap';
 import axios from 'axios';
-import { NavDropdown, Navbar, Nav } from "react-bootstrap";
+import { NavDropdown, Navbar, Nav, Modal } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const Placedetail = props => {
+    const [booking, setBooking] = useState({ nights: "", persons: "", city: "", zip: "", checkIn: "", checkOut: "" })
+    const [modal, setModal] = useState(false)
+    const openModal = () => setModal(true)
+    const closeModal = () => setModal(false)
     const [data, setData] = useState(null)
     useEffect(() => {
-        axios.get(`/place/${props.match.params.id}`).then(res => setData(res.data))
+        axios.get(`/place/${props.match.params.id}`).then(res => {
+            setData(res.data)
+            setBooking({ ...booking, city: res.data.city, zip: res.data.zip })
+        })
     }, [])
     const history = useHistory();
     const onLogout = () => {
@@ -28,6 +37,24 @@ const Placedetail = props => {
     const handleEditProfile = () => {
         history.push("/editProfile", { type: "user" });
     };
+    const handleChange = e => {
+        setBooking({ ...booking, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        axios.post('/book/places', {
+            total: (booking.nights * data.price) * booking.persons,
+            checkIn: booking.checkIn,
+            checkOut: booking.checkOut,
+            city: booking.city,
+            zip: booking.zip,
+        }).then(res => {
+            closeModal();
+            toast.success("Booking details sent to email")
+        }).catch(err => {
+            toast.error("Something's wrong apoligies")
+        })
+    }
     return (
         <Fragment>
             <nav>
@@ -60,8 +87,8 @@ const Placedetail = props => {
                                 <div className="col-md-6 offset-md-3">
                                     <Carousel>
                                         {
-                                            data.images.map(url =>
-                                                <Carousel.Item>
+                                            data.images.map((url,idx) =>
+                                                <Carousel.Item key={idx}>
                                                     <img
                                                         className="d-block w-100"
                                                         alt="..."
@@ -82,12 +109,71 @@ const Placedetail = props => {
                                     <p>Can have upto {data.persons} persons</p>
                                     <p className="lead"><strong>${data.price}</strong> / night</p>
                                     <p className="lead">{data.city} - {data.state} - {data.zip}</p>
-                                    <button className="btn btn-danger">Book Now</button>
-                    </div>
+                                    <button className="btn btn-danger" onClick={openModal}>Book Now</button>
+                                </div>
                             </div>
                         </div>
                     </div>
             }
+            <Modal show={modal} onHide={closeModal} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Fill details for booking</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group row">
+                            <label htmlFor="nights" className="col-6 col-form-label">Total nights</label>
+                            <div className="col-6">
+                                <input
+                                    className="form-control"
+                                    type="number"
+                                    name="nights"
+                                    value={booking.nights}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="persons" className="col-6 col-form-label">Total persons</label>
+                            <div className="col-6">
+                                <input
+                                    className="form-control"
+                                    type="number"
+                                    name="persons"
+                                    value={booking.persons}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="checkIn" className="col-6 col-form-label">Check In</label>
+                            <div className="col-6">
+                                <input
+                                    className="form-control"
+                                    type="date"
+                                    name="checkIn"
+                                    value={booking.checkIn}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="checkOut" className="col-6 col-form-label">Check Out</label>
+                            <div className="col-6">
+                                <input
+                                    className="form-control"
+                                    type="date"
+                                    name="checkOut"
+                                    value={booking.checkOut}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <button className="btn btn-danger" type="submit">Confirm Booking</button>
+                    </form>
+                </Modal.Body>
+            </Modal>
+            <ToastContainer />
         </Fragment>
     );
 }
